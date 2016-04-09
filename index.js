b3be319ecd121963b24dc5cf90dfcae48b18e53d
Tomek Fijoł <tomekfijol@gmail.com>
2016-04-09 12:13:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,71 @@
-function deleteFromGlobal(name) {
-  try {
-    delete window[name];
-  } catch (e) {
-    window[name] = null;
+/* global define */
+
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  } else {
+    root.SimpleLoadScript = factory();
   }
-}
+}(this, function () {
 
-// array of urls or array of objects
-function all() {
-  if (!arguments.length) return Promise.reject(new Error('No files or no file configs'));
-  return Promise.all(Array.prototype.slice.call(arguments).map(getScript));
-}
-
-function getScript(url, options) {
-  return new Promise(function (resolve, reject) {
-    if (typeof url === 'object') {
-      options = url;
-      url = options.url;
+  function deleteFromGlobal(name) {
+    try {
+      delete window[name];
+    } catch (e) {
+      window[name] = null;
     }
-    if (!options) options = {};
-    if (!url) {
-      reject('Error: no script url');
-      return;
-    }
-    var script = document.createElement('script');
-    var where = options.inBody ? document.body : document.head;
-    var attrs = options.attrs;
-    var callBackName = options.callBackName;
-    for (var attr in attrs) {
-      if (Object.prototype.hasOwnProperty.call(attrs, attr)) {
-        script.setAttribute(attr, attrs[attr]);
+  }
+  
+  // array of urls or array of objects
+  function all() {
+    if (!arguments.length) return Promise.reject(new Error('No files or no file configs'));
+    return Promise.all(Array.prototype.slice.call(arguments).map(getScript));
+  }
+  
+  function getScript(url, options) {
+    return new Promise(function (resolve, reject) {
+      if (typeof url === 'object') {
+        options = url;
+        url = options.url;
       }
-    }
-    if (!callBackName) {
-      script.addEventListener('load', function(e) {
-        resolve(script);
+      if (!options) options = {};
+      if (!url) {
+        reject('Error: no script url');
+        return;
+      }
+      var script = document.createElement('script');
+      var where = options.inBody ? document.body : document.head;
+      var attrs = options.attrs;
+      var callBackName = options.callBackName;
+      for (var attr in attrs) {
+        if (Object.prototype.hasOwnProperty.call(attrs, attr)) {
+          script.setAttribute(attr, attrs[attr]);
+        }
+      }
+      if (!callBackName) {
+        script.addEventListener('load', function(e) {
+          resolve(script);
+        });
+      } else {
+        window[callBackName] = function() {
+          deleteFromGlobal(callBackName);
+          resolve(script);
+        };
+      }
+      script.addEventListener('error', function(e) {
+        where.removeChild(script);
+        reject('Error: loading script');
       });
-    } else {
-      window[callBackName] = function() {
-        deleteFromGlobal(callBackName);
-        resolve(script);
-      };
-    }
-    script.addEventListener('error', function(e) {
-      where.removeChild(script);
-      reject('Error: loading script');
+      script.src = url;
+      where.appendChild(script);
     });
-    script.src = url;
-    where.appendChild(script);
-  });
-}
+  }
+  
+  getScript.deleteFromGlobal = deleteFromGlobal;
+  getScript.all = all;
+  
+  return getScript;
 
-getScript.deleteFromGlobal = deleteFromGlobal;
-getScript.all = all;
-
-module.exports = getScript;
+}});
