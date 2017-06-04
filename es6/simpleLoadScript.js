2b5fb2cf344c1f82b5da6a497d20b7af7ef9594f
Tomasz Fijoł <tomekfijol@gmail.com>
2017-06-04 09:49:20 +0200

    moar es6, screw ie <9

--- es6/simpleLoadScript.js
+++ es6/simpleLoadScript.js
@@ -1,84 +1,73 @@
-(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define([], factory);
-  } else if (typeof module === 'object' && module.exports) {
-    module.exports = factory();
-  } else {
-    root.simpleLoadScript = factory();
-  }
-}(this, function () {
-  var undef;
-
-  function deleteFromGlobal(name) {
-    try {
-      delete window[name];
-    } catch (e) {
-      window[name] = null;
+export default function getScript(url, options) {
+  return new Promise(function (resolve, reject) {
+    if (typeof url === 'object') {
+      options = url;
+      url = options.url;
     }
-  }
+    if (!options) {
+      options = {};
+    }
+    if (!url) {
+      reject('Error: no script url');
+      return;
+    }
 
-  function getScript(url, options) {
-    return new Promise(function (resolve, reject) {
-      if (typeof url === 'object') {
-        options = url;
-        url = options.url;
+    const script = document.createElement('script');
+    const where = (function () {
+      if (options.insertInto) {
+        return document.querySelector(options.insertInto);
       }
-      if (!options) options = {};
-      if (!url) {
-        reject('Error: no script url');
-        return;
+      return options.inBody ? document.body : document.head;
+    }());
+
+    if (!where) {
+      reject('Error: no DOM element to append script');
+      return;
+    }
+
+    const attrs = options.attrs;
+    const removeScript = options.removeScript;
+    const callBackName = options.callBackName;
+
+    for (const attr in attrs) {
+      if (Object.prototype.hasOwnProperty.call(attrs, attr)) {
+        script.setAttribute(attr, attrs[attr]);
       }
-      var script = document.createElement('script');
-      var where = (function () {
-        if (options.insertInto) {
-          return document.querySelector(options.insertInto);
-        }
-        return options.inBody ? document.body : document.head;
-      }());
-      if (!where) {
-        reject('Error: no DOM element to append script');
-        return;
-      }
-      var attrs = options.attrs;
-      var removeScript = options.removeScript;
-      var callBackName = options.callBackName;
-      for (var attr in attrs) {
-        if (Object.prototype.hasOwnProperty.call(attrs, attr)) {
-          script.setAttribute(attr, attrs[attr]);
-        }
-      }
-      if (!callBackName) {
-        script.addEventListener('load', function () {
-          if (removeScript) where.removeChild(script);
-          resolve(removeScript ? undef : script);
-        });
-      } else {
-        window[callBackName] = function (res) {
-          if (!res) res = removeScript ? undef : script;
-          if (!options.dontRemoveCallBack) deleteFromGlobal(callBackName);
-          if (removeScript) where.removeChild(script);
-          resolve(res || removeScript ? undef : script);
-        };
-      }
-      script.addEventListener('error', function () {
-        where.removeChild(script);
-        reject('Error: loading script');
+    }
+    if (!callBackName) {
+      script.addEventListener('load', function () {
+        if (removeScript) where.removeChild(script);
+        resolve(removeScript ? undefined : script);
       });
-      script.src = url;
-      where.appendChild(script);
+    } else {
+      window[callBackName] = function (res) {
+        if (!res) {
+          res = removeScript ? undefined : script;
+        }
+        if (!options.dontRemoveCallBack) {
+          delete window[callBackName];
+        }
+        if (removeScript) {
+          where.removeChild(script);
+        }
+        resolve(res || removeScript ? undefined : script);
+      };
+    }
+    script.addEventListener('error', function () {
+      where.removeChild(script);
+      reject('Error: loading script');
     });
+    script.src = url;
+    where.appendChild(script);
+  });
+}
+
+function all() {
+  if (!arguments.length) {
+    return Promise.reject(new Error('No files or no file configs'));
   }
+  return Promise.all([...arguments].map(e => Array.isArray(e) ? getScript(...e) : getScript(e)));
+}
 
-  // array of urls or array of objects
-  function all() {
-    if (!arguments.length) return Promise.reject(new Error('No files or no file configs'));
-    return Promise.all(Array.prototype.slice.call(arguments).map(function (e) {
-      return Array.isArray(e) ? getScript.apply(null, e) : getScript(e);
-    }));
-  }
+getScript.all = all;
 
-  getScript.deleteFromGlobal = deleteFromGlobal;
-  getScript.all = all;
-
-  return getScript;
-}));
