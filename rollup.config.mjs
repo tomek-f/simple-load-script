7cba422287796f290ec9ec9e8a4f9d5248077481
Tomek Fijoł <tomekfijol@gmail.com>
2023-05-03 12:38:25 +0200

    feat: remove @rollup/plugin-commonjs & dedupe (#47)

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,9 @@
-import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 
 export default {
   input: './src/index.ts',
-  plugins: [commonjs(), terser(), typescript({ include: ['./src/index.ts'] })],
+  plugins: [terser(), typescript({ include: ['./src/index.ts'] })],
   output: [
     { file: './dist/index.cjs.js', format: 'cjs', exports: 'default' },
     { file: './dist/index.es.js', format: 'es' },
