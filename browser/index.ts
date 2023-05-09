7520d0eefbfe105c100c057627307537ddf5fb8e
Tomek Fijoł <tomekfijol@gmail.com>
2023-05-09 09:51:25 +0200

    feat: basic tests and other changes (#51)

--- browser/index.ts
+++ browser/index.ts
@@ -1,28 +1,27 @@
+import simpleLoadScript from '../src/index.js';
+
 declare global {
   interface Window {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    simpleLoadScript: (...args: any[]) => Promise<any>;
+    simpleLoadScript: typeof simpleLoadScript;
   }
 }
 
-export {};
-
-import simpleLoadScript from '../src/index.js';
-
 window.simpleLoadScript = simpleLoadScript;
 
-const root = document.createElement('div');
-const button = document.createElement('button');
-button.id = 'btn';
+// const root = document.createElement('div');
+// const button = document.createElement('button');
+// button.id = 'btn';
 
-let count = 0;
+// let count = 0;
 
-button.textContent = `Clicked ${count} time(s)`;
+// button.textContent = `Clicked ${count} time(s)`;
 
-button.onclick = () => {
-  count++;
-  button.textContent = `Clicked ${count} time(s)`;
-};
+// button.onclick = () => {
+//   count++;
+//   button.textContent = `Clicked ${count} time(s)`;
+// };
 
-root.appendChild(button);
-document.body.appendChild(root);
+// root.appendChild(button);
+// document.body.appendChild(root);
+
+export {};
--- test/basic.spec.ts
+++ test/basic.spec.ts
@@ -1,43 +1,74 @@
-import { afterAll, beforeAll, describe, expect, test } from 'vitest';
+import { afterAll, beforeAll, expect, test } from 'vitest';
 import { preview } from 'vite';
 import type { PreviewServer } from 'vite';
 import { Browser, Page, chromium } from 'playwright';
 
-// https://github.com/vitest-dev/vitest/blob/main/examples/puppeteer/test/basic.test.ts
 // https://gist.github.com/mizchi/5f67109d0719ef6dd57695e1f528ce8d
 
-describe(`browser:${chromium.name()}`, () => {
-  let browser: Browser;
-  let server: PreviewServer;
-  let page: Page;
+let browser: Browser;
+let server: PreviewServer;
+let page: Page;
 
-  beforeAll(async () => {
-    browser = await chromium.launch({ headless: true });
-    server = await preview({ preview: { port: 3000 } });
-    page = await browser.newPage();
-  });
-
-  afterAll(async () => {
-    await browser.close();
-    await new Promise<void>((resolve, reject) => {
-      server.httpServer.close((error) => (error ? reject(error) : resolve()));
-    });
-  });
-
-  test('should have the correct title', async () => {
-    try {
-      await page.goto('http://localhost:3000');
-      const button = await page.$('#btn');
-      expect(button).toBeDefined();
-
-      let text = await page.evaluate((btn) => btn?.textContent, button);
-      expect(text).toBe('Clicked 0 time(s)');
-
-      await button?.click();
-      text = await page.evaluate((btn) => btn?.textContent, button);
-    } catch (e) {
-      console.error(e);
-      expect(e).toBeUndefined();
-    }
-  }, 60_000);
+beforeAll(async () => {
+  browser = await chromium.launch({ headless: true });
+  server = await preview({ preview: { port: 3000 } });
+  page = await browser.newPage();
 });
+
+afterAll(async () => {
+  await browser.close();
+  await new Promise<void>((resolve, reject) => {
+    server.httpServer.close((error) => (error ? reject(error) : resolve()));
+  });
+});
+
+// test('should have the correct title', async () => {
+//   try {
+//     await page.goto('http://localhost:3000');
+//     const button = await page.$('#btn');
+//     expect(button).toBeDefined();
+
+//     let text = await page.evaluate((btn) => btn?.textContent, button);
+//     expect(text).toBe('Clicked 0 time(s)');
+
+//     await button?.click();
+//     text = await page.evaluate((btn) => btn?.textContent, button);
+//   } catch (err) {
+//     console.error(err);
+//     expect(err).toBeUndefined();
+//   }
+// }, 60_000);
+
+test('proper load', async () => {
+  try {
+    await page.goto('http://localhost:3000');
+
+    const ref = await page.evaluate(async () => {
+      const scriptRef = await window.simpleLoadScript(
+        '//code.jquery.com/jquery-2.2.3.js',
+      );
+      return scriptRef;
+    });
+
+    expect(ref).toBeDefined(); // HTMLScriptElement
+  } catch (err) {
+    expect(err).toBeUndefined();
+  }
+}, 60_000);
+
+test('error load', async () => {
+  try {
+    await page.goto('http://localhost:3000');
+
+    const ref = await page.evaluate(async () => {
+      const scriptRef = await window.simpleLoadScript(
+        '//wrong.domain/jquery-2.2.3.js',
+      );
+      return scriptRef;
+    });
+
+    expect(ref).toBeDefined();
+  } catch (err) {
+    expect((err as Error).message.includes('Error: Loading script')).toBe(true);
+  }
+}, 60_000);
