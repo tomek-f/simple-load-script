0988142d4c5a2a852a49387de5a8136d148509b5
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-09-06 22:58:42 +0200

    feat: Change indent to 4 spaces

--- test/removeScript.spec.ts
+++ test/removeScript.spec.ts
@@ -9,62 +9,62 @@ let server: PreviewServer;
 let page: Page;
 
 beforeAll(async () => {
-  browser = await chromium.launch({ headless: true });
-  server = await preview({ preview: { port: 3004 } });
-  page = await browser.newPage();
+    browser = await chromium.launch({ headless: true });
+    server = await preview({ preview: { port: 3004 } });
+    page = await browser.newPage();
 });
 
 afterAll(async () => {
-  await browser.close();
-  await new Promise<void>((resolve, reject) => {
-    server.httpServer.close((error) => (error ? reject(error) : resolve()));
-  });
+    await browser.close();
+    await new Promise<void>((resolve, reject) => {
+        server.httpServer.close((error) => (error ? reject(error) : resolve()));
+    });
 });
 
 test(
-  'removeScript true',
-  async () => {
-    try {
-      await page.goto('http://localhost:3004');
-      await page.evaluate(async () => {
-        await window.simpleLoadScript({
-          url: '//code.jquery.com/jquery-2.2.3.js',
-          removeScript: true,
-          attrs: { id: 'jquery' },
-        });
-      });
-      const jquery = await page.$('script#jquery');
-      const id = await page.evaluate((script) => script?.id, jquery);
-      const nodeType = await page.evaluate(
-        (script) => script?.nodeType,
-        jquery,
-      );
-      expect(id).toBeUndefined();
-      expect(nodeType).toBeUndefined();
-    } catch (err) {
-      expect(err).toBeUndefined();
-    }
-  },
-  TIMEOUT,
+    'removeScript true',
+    async () => {
+        try {
+            await page.goto('http://localhost:3004');
+            await page.evaluate(async () => {
+                await window.simpleLoadScript({
+                    url: '//code.jquery.com/jquery-2.2.3.js',
+                    removeScript: true,
+                    attrs: { id: 'jquery' },
+                });
+            });
+            const jquery = await page.$('script#jquery');
+            const id = await page.evaluate((script) => script?.id, jquery);
+            const nodeType = await page.evaluate(
+                (script) => script?.nodeType,
+                jquery,
+            );
+            expect(id).toBeUndefined();
+            expect(nodeType).toBeUndefined();
+        } catch (err) {
+            expect(err).toBeUndefined();
+        }
+    },
+    TIMEOUT,
 );
 
 test(
-  'removeScript false',
-  async () => {
-    try {
-      await page.goto('http://localhost:3004');
-      await page.evaluate(async () => {
-        await window.simpleLoadScript({
-          url: '//code.jquery.com/jquery-2.2.3.js',
-          attrs: { id: 'jquery' },
-        });
-      });
-      const jquery = await page.$('script#jquery');
-      const id = await page.evaluate((script) => script?.id, jquery);
-      expect(id).toBe('jquery');
-    } catch (err) {
-      expect(err).toBeUndefined();
-    }
-  },
-  TIMEOUT,
+    'removeScript false',
+    async () => {
+        try {
+            await page.goto('http://localhost:3004');
+            await page.evaluate(async () => {
+                await window.simpleLoadScript({
+                    url: '//code.jquery.com/jquery-2.2.3.js',
+                    attrs: { id: 'jquery' },
+                });
+            });
+            const jquery = await page.$('script#jquery');
+            const id = await page.evaluate((script) => script?.id, jquery);
+            expect(id).toBe('jquery');
+        } catch (err) {
+            expect(err).toBeUndefined();
+        }
+    },
+    TIMEOUT,
 );
