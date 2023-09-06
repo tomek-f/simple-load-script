98fd8f3df0492cec271d2a9158f6b526ec12116b
Tomasz Fijoł <tomasz.fijol@bold.com>
2023-09-06 23:03:00 +0200

    up

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,14 +2,18 @@ import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 
 export default {
-  input: './src/index.ts',
-  plugins: [terser(), typescript({ include: ['./src/index.ts'] })],
-  output: [
-    { file: './dist/index.cjs.js', format: 'cjs', exports: 'default' },
-    { file: './dist/index.es.js', format: 'es' },
-    { file: './dist/index.umd.js', format: 'umd', name: 'simpleLoadScript' },
-  ],
-  watch: {
-    include: './src/**',
-  },
+    input: './src/index.ts',
+    plugins: [terser(), typescript({ include: ['./src/index.ts'] })],
+    output: [
+        { file: './dist/index.cjs.js', format: 'cjs', exports: 'default' },
+        { file: './dist/index.es.js', format: 'es' },
+        {
+            file: './dist/index.umd.js',
+            format: 'umd',
+            name: 'simpleLoadScript',
+        },
+    ],
+    watch: {
+        include: './src/**',
+    },
 };
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,8 @@ import { UserConfigExport, defineConfig } from 'vite';
 import { TIMEOUT } from './test/constants';
 
 export default defineConfig({
-  test: {
-    testTimeout: TIMEOUT,
-    hookTimeout: TIMEOUT,
-  },
+    test: {
+        testTimeout: TIMEOUT,
+        hookTimeout: TIMEOUT,
+    },
 }) satisfies UserConfigExport;
